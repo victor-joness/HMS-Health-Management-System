bf6ce7de9e89022841a363df6429cfa5ecd6b011
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-12-17 09:17:44 -0300

    Create server.js

---
+++ Server/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const users = require("./routes/users");
+const doutores = require("./routes/doutores");
+const enfermeiras = require("./routes/Enfermeira");
+const register = require("./routes/register");
+const login = require("./routes/login");
+const doadores = require("./routes/doadores");
+const cirurgia = require("./routes/cirurgias");
+
+const multer = require("multer");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//ENDPOINTS
+app.use("/api/register", register);
+app.use("/api/login", login);
+app.use("/api/users", users);
+app.use("/api/doutores", doutores);
+app.use("/api/doadores", doadores);
+app.use("/api/enfermeiras", enfermeiras);
+app.use("/api/cirurgia", cirurgia);
+
+//Multer Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome the api ERP-Hospitalar");
+});
+
+module.exports = app;
