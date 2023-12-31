4b1227b635d94845b8de17033b59b387a0eaef34
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-12-31 08:47:20 -0300

    Create App.jsx

---
+++ Client/src/App.jsx
@@ -0,0 +1,58 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
+
+import Navbar from "./Components/Navbar/Navbar";
+
+//Pages
+import NotFound from "./Pages/NotFound/NotFound";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+//Admin pages
+import Doutor from "./Pages/Admin/Doutor/Doutor";
+import Enfermeira from "./Pages/Admin/Enfermeira/Enfermeira";
+import Camas from "./Pages/Admin/Camas/Camas";
+//Home
+import Home from "./Pages/Home/Home";
+//Doutor
+import DoutorDoador from "./Pages/Doutor/Doutor-Doador/DoutorDoador";
+import DoutorBanco from "./Pages/Doutor/Doutor-banco/DoutorBanco";
+import DoutorDespachar from "./Pages/Doutor/Doutor-despachar/DoutorDespachar";
+import DoutorOperacao from "./Pages/Doutor/Doutor-operações/DoutorOperacao";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ToastContainer />
+        
+        <div className="content-container">
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Admin rotas */}
+            <Route path="/admin/doutor" element={<Doutor />} />
+            <Route path="/admin/enfermeira" element={<Enfermeira />} />
+            <Route path="/admin/camas" element={<Camas />} />
+
+            {/* Doutor rotas */}
+            <Route path="/doutor/doador" element={<DoutorDoador />} />
+            <Route path="/doutor/banco" element={<DoutorBanco />} />
+            <Route path="/doutor/despachar" element={<DoutorDespachar />} />
+            <Route path="/doutor/operacoes" element={<DoutorOperacao />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
