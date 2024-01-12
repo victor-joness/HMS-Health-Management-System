b1e6db4a952e0fe29db8d378c0c422a6dc6366a3
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-01-12 18:49:34 -0300

    Update index.spec.js

--- Server/index.spec.js
+++ Server/index.spec.js
@@ -43,24 +43,6 @@ describe("API Tests Login", () => {
   });
 });
 
-describe("API Tests Users", () => {
-  it("Get all users", async () => {
-    const res = await request(app).get("/api/users");
 
-    let usersBoolean = false;
-
-    if (res.body.length > 0) {
-      usersBoolean = true;
-    }
-
-    expect(res.status).toBe(200);
-    expect(usersBoolean).toBe(true);
-
-    /*
-    expect(usersBoolean).toEqual(true);
-    expect(usersBoolean).toHaveProperty(true);
-    */
-  });
-});
 
 describe("API Tests Registro", () => {});
