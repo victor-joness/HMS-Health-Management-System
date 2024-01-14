401499e9cda5d6d675d23e5c67739bbdb10a01e1
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-01-14 10:11:56 -0300

    Update Home.jsx

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 
 import logo from "../../Assets/LOGO.png";
+import banner from "../../Assets/banner.png"
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
@@ -16,19 +17,26 @@ const Home = () => {
   };
 
   return (
-    <div className="container-home">
-      <div className="container-home-center">
-        <div className="image">
-          <img src={logo} alt="" />
-        </div>
-        <h1>Bem Vindo ao ERP</h1>
-        <div className="buttons">
-          <button className="container-home-login" onClick={handleLogin}>
-            Login
-          </button>
-          <button className="container-home-login" onClick={handleRegister}>
-            Register
-          </button>
+    <div className="mainLoginPage">
+      <div className="leftside">
+        <img src={banner} alt="banner" />
+      </div>
+      <div className="rightside">
+        <div className="container-home">
+          <div className="container-home-center">
+            <div className="image">
+              <img src={logo} alt="" />
+            </div>
+            <h1>Bem Vindo ao ERP</h1>
+            <div className="buttons">
+              <button className="container-home-login" onClick={handleLogin}>
+                Login
+              </button>
+              <button className="container-home-login" onClick={handleRegister}>
+                Register
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
