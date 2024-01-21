4b727feb4aadf400238959c98500ae631fda454a
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-01-21 10:45:16 -0300

    Create Paciente.jsx

---
+++ Client/src/Components/Paciente/Paciente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Paciente.css";
+
+const Paciente = () => {
+  return (
+    <div>
+      <h1>TESTE DE PACIENTES</h1>
+    </div>
+  )
+}
+
+export default Paciente
