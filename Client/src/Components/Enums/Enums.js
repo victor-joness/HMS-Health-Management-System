85c7c1c63ae305e9b0bc10f359272707a582201a
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-01-27 15:42:31 -0300

    Create Enums.js

---
+++ Client/src/Components/Enums/Enums.js
@@ -0,0 +1,33 @@
+/**
+ * Tipo Sanguineo do paciente
+ */
+export const TipoSanguineo = {
+    A_POSITIVO: "A_POSITIVO",
+    A_NEGATIVO: "A_NEGATIVO",
+    B_POSITIVO: "B_POSITIVO",
+    B_NEGATIVO: "B_NEGATIVO",
+    O_POSITIVO: "O_POSITIVO",
+    O_NEGATIVO: "O_NEGATIVO",
+    AB_POSITIVO: "AB_POSITIVO",
+    AB_NEGATIVO: "AB_NEGATIVO"
+}
+
+/**
+ * Gênero do paciente
+ */
+export const Genero = {
+    MASCULINO: "MASCULINO",
+    FEMININO: "FEMININO",
+    OUTRO: "OUTRO"
+}
+
+/**
+ * Nivel de dor do paciente
+ */
+export const DorStatus = {
+    NENHUM: "NENHUM",
+    LEVE: "LEVE",
+    NORMAL: "NORMAL",
+    ALTA: "ALTA",
+    CRITICA: "CRITICA"
+}
