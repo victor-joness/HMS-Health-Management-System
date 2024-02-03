dae28ea28cdb0038d66c32e9536c5911a79d9f71
Victor Mesquita <victorjonesmesquits@gmail.com>
2024-02-03 13:31:05 -0300

    Create PacientesContainer.jsx

---
+++ Client/src/Components/PacientesContainer/PacientesContainer.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import PacienteCard from "../PacienteCard/PacienteCard";
+import PaginationComponent from "../Pagination/Pagination";
+
+const PacientesContainer = ({ pacientesData }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 15; // Número de cards por página
+
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentPacientes = pacientesData.slice(
+    indexOfFirstCard,
+    indexOfLastCard
+  );
+  const totalPages = Math.ceil(pacientesData.length / cardsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      <div className="container-cards">
+        {currentPacientes.map((paciente) => (
+          <PacienteCard key={paciente.pacienteId} paciente={paciente} />
+        ))}
+
+        <PaginationComponent
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PacientesContainer;
